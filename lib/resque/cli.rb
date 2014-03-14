59779a99278579ab1770fbb452ba1a615d20ad24
Ming Liu <liuming@lmws.net>
2014-03-14 12:33:59 -0700

    basic commands for queue and worker

--- lib/resque/cli.rb
+++ lib/resque/cli.rb
@@ -1,9 +1,60 @@
 require_relative "cli/version"
 require 'thor'
+require 'resque'
 
 module Resque
   module Cli
-    class ResqueCli < ::Thor
+    class Queue < ::Thor
+      desc "list", "list all queues"
+      def list
+        puts ::Resque.queues
+      end
+
+      desc "remove", "remove a queue"
+      def remove(queue_name)
+        puts ::Resque.remove_queue(queue_name)
+      end
+
+      desc "peek", "peek into a queue"
+      def peek(queue_name, start = 0, count = 1)
+        puts ::Resque.peek(queue_name, start, count)
+      end
+
+      desc "size", "size of a queue"
+      def size(queue_name)
+        puts ::Resque.size(queue_name)
+      end
+    end
+
+    class Worker < ::Thor
+      desc "list", "list all workers"
+      def list
+        puts ::Resque.workers
+      end
+
+      desc "working", "list all working workers"
+      def working
+        puts ::Resque.working
+      end
+
+      desc "remove", "remove a queue"
+      def remove(worker_id)
+        puts ::Resque.remove_worker(worker_id)
+      end
+
+    end
+
+    class Commander < ::Thor
+      desc "info", "resque runtime info"
+      def info
+        puts ::Resque.info
+      end
+
+      desc "queue", "queue commands"
+      subcommand 'queue', Queue
+
+      desc "worker", "worker commands"
+      subcommand 'worker', Worker
     end
   end
 end
